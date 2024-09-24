082bd0fbccc9fa355c852f677164ebc0ece04b14
Silas Stilling-Jørgensen <sis002@edu.zealand.dk>
2024-09-24 23:21:32 +0200

    Tilføjet Random, Add og Subtract Metoder

--- TCPServer/Program.cs
+++ TCPServer/Program.cs
@@ -3,6 +3,9 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Reflection.Metadata;
+using System.Runtime;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 Console.WriteLine("TcpServerTest");
@@ -35,8 +38,56 @@ void HandleClient(TcpClient socket)
         writer.Flush();
         if (message == "stop")
         {
+            writer.WriteLine("Server shutting down");
+            writer.Flush();
             socket.Close();
         }
+        if (message == "help")
+        {
+            writer.WriteLine("Hello World\n Following commands are available\n 1. Random\n 2. Add\n 3. Subtract");
+            writer.Flush();
+        }
+        if (message == "Random")
+        {
+            writer.WriteLine("Enter the minimum number:");
+            writer.Flush();
+            int min = Convert.ToInt32(reader.ReadLine());
+
+            writer.WriteLine("Enter the maximum number:");
+            writer.Flush();
+            int max = Convert.ToInt32(reader.ReadLine());
+
+            Random random = new Random();
+            int randomNumber = random.Next(min, max + 1);
+
+            writer.WriteLine(randomNumber);
+            writer.Flush();
+        }
+        if (message == "Add")
+        {
+            writer.WriteLine("Enter first number");
+            writer.Flush();
+            int num1 = Convert.ToInt32(reader.ReadLine());
+            writer.WriteLine("Enter second number");
+            writer.Flush();
+            int num2 = Convert.ToInt32(reader.ReadLine());
+            int sum = num1 + num2;
+            writer.WriteLine(sum);
+            writer.Flush();
+        }
+        if (message == "Subtract")
+        {
+            writer.WriteLine("Enter first number");
+            writer.Flush();
+            int num1 = Convert.ToInt32(reader.ReadLine());
+            writer.WriteLine("Enter second number");
+            writer.Flush();
+            int num2 = Convert.ToInt32(reader.ReadLine());
+            int sub = num1 - num2;
+            writer.WriteLine(sub);
+            writer.Flush();
+        }
+
     }
 
     Console.WriteLine("stop");
